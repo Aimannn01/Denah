bd3575690772985aabe88ef2df8dec8fedc62ca1
Aimannn01 <Aimanyazid746@gmail.com>
2026-02-06 11:37:58 +0800

    proses

--- Database/api_stats.php
+++ Database/api_stats.php
@@ -7,6 +7,10 @@ $db   = "bbm";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
+if ($conn->connect_error) {
+    die(json_encode(['status' => 'error', 'message' => 'koneksi gagal']));
+}
+
 $sql = "SELECT COUNT() as total FROM bbm WHERE bidang = 'IT' ";
 $result = $conn->query($sql);
 $data = $result->fetch_assoc();
